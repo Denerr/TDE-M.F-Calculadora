0241f545707ac6d2a94d619c9969d7272d88d9e1
Dener Cerantola <denercerantola3@hotmail.com>
2021-04-13 20:45:47 -0300

    Adicionado Tela de Calculo de Taxa;

--- TDE-M.F-Calculadora/Form4.Designer.cs
+++ TDE-M.F-Calculadora/Form4.Designer.cs
@@ -29,19 +29,169 @@ namespace TDE_M.F_Calculadora
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.lblJuros = new System.Windows.Forms.Label();
+            this.lblCapital = new System.Windows.Forms.Label();
+            this.lblTempo = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnPercent = new System.Windows.Forms.Button();
+            this.txtJuros = new System.Windows.Forms.TextBox();
+            this.txtCapital = new System.Windows.Forms.TextBox();
+            this.txtTempo = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.txtResultado.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold);
+            this.txtResultado.Location = new System.Drawing.Point(270, 100);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(202, 34);
+            this.txtResultado.TabIndex = 3;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnResultado.FlatAppearance.BorderSize = 2;
+            this.btnResultado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResultado.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnResultado.ForeColor = System.Drawing.Color.White;
+            this.btnResultado.Location = new System.Drawing.Point(270, 172);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(114, 42);
+            this.btnResultado.TabIndex = 4;
+            this.btnResultado.Text = "Resolver";
+            this.btnResultado.UseVisualStyleBackColor = true;
+            this.btnResultado.Click += new System.EventHandler(this.btnResultado_Click);
+            this.btnResultado.MouseEnter += new System.EventHandler(this.btnResultado_MouseEnter);
+            this.btnResultado.MouseLeave += new System.EventHandler(this.btnResultado_MouseLeave);
+            // 
+            // lblJuros
+            // 
+            this.lblJuros.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblJuros.AutoSize = true;
+            this.lblJuros.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJuros.ForeColor = System.Drawing.Color.White;
+            this.lblJuros.Location = new System.Drawing.Point(33, 26);
+            this.lblJuros.Name = "lblJuros";
+            this.lblJuros.Size = new System.Drawing.Size(63, 27);
+            this.lblJuros.TabIndex = 5;
+            this.lblJuros.Text = "Juros";
+            // 
+            // lblCapital
+            // 
+            this.lblCapital.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblCapital.AutoSize = true;
+            this.lblCapital.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCapital.ForeColor = System.Drawing.Color.White;
+            this.lblCapital.Location = new System.Drawing.Point(32, 100);
+            this.lblCapital.Name = "lblCapital";
+            this.lblCapital.Size = new System.Drawing.Size(75, 27);
+            this.lblCapital.TabIndex = 6;
+            this.lblCapital.Text = "Capital";
+            // 
+            // lblTempo
+            // 
+            this.lblTempo.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblTempo.AutoSize = true;
+            this.lblTempo.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTempo.ForeColor = System.Drawing.Color.White;
+            this.lblTempo.Location = new System.Drawing.Point(33, 174);
+            this.lblTempo.Name = "lblTempo";
+            this.lblTempo.Size = new System.Drawing.Size(71, 27);
+            this.lblTempo.TabIndex = 7;
+            this.lblTempo.Text = "Tempo";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.Color.White;
+            this.lblResultado.Location = new System.Drawing.Point(319, 49);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(100, 27);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // btnPercent
+            // 
+            this.btnPercent.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnPercent.FlatAppearance.BorderSize = 2;
+            this.btnPercent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPercent.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnPercent.ForeColor = System.Drawing.Color.White;
+            this.btnPercent.Location = new System.Drawing.Point(415, 172);
+            this.btnPercent.Name = "btnPercent";
+            this.btnPercent.Size = new System.Drawing.Size(57, 42);
+            this.btnPercent.TabIndex = 9;
+            this.btnPercent.Text = "%";
+            this.btnPercent.UseVisualStyleBackColor = true;
+            this.btnPercent.Click += new System.EventHandler(this.btnPercent_Click);
+            this.btnPercent.MouseEnter += new System.EventHandler(this.btnPercent_MouseEnter);
+            this.btnPercent.MouseLeave += new System.EventHandler(this.btnPercent_MouseLeave);
+            // 
+            // txtJuros
+            // 
+            this.txtJuros.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtJuros.Location = new System.Drawing.Point(38, 56);
+            this.txtJuros.Name = "txtJuros";
+            this.txtJuros.Size = new System.Drawing.Size(167, 20);
+            this.txtJuros.TabIndex = 0;
+            // 
+            // txtCapital
+            // 
+            this.txtCapital.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtCapital.Location = new System.Drawing.Point(37, 130);
+            this.txtCapital.Name = "txtCapital";
+            this.txtCapital.Size = new System.Drawing.Size(167, 20);
+            this.txtCapital.TabIndex = 1;
+            // 
+            // txtTempo
+            // 
+            this.txtTempo.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtTempo.Location = new System.Drawing.Point(37, 204);
+            this.txtTempo.Name = "txtTempo";
+            this.txtTempo.Size = new System.Drawing.Size(167, 20);
+            this.txtTempo.TabIndex = 2;
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(8)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(535, 288);
+            this.Controls.Add(this.btnPercent);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblTempo);
+            this.Controls.Add(this.lblCapital);
+            this.Controls.Add(this.lblJuros);
+            this.Controls.Add(this.btnResultado);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtTempo);
+            this.Controls.Add(this.txtCapital);
+            this.Controls.Add(this.txtJuros);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "Form4";
             this.Text = "Calculo de Taxa";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnResultado;
+        private System.Windows.Forms.Label lblJuros;
+        private System.Windows.Forms.Label lblCapital;
+        private System.Windows.Forms.Label lblTempo;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnPercent;
+        private System.Windows.Forms.TextBox txtJuros;
+        private System.Windows.Forms.TextBox txtCapital;
+        private System.Windows.Forms.TextBox txtTempo;
     }
 }
--- TDE-M.F-Calculadora/Form4.cs
+++ TDE-M.F-Calculadora/Form4.cs
@@ -16,5 +16,54 @@ namespace TDE_M.F_Calculadora
         {
             InitializeComponent();
         }
+
+        public double resultado, juros, capital, tempo;
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+
+            juros = Double.Parse(txtJuros.Text);
+            capital = Double.Parse(txtCapital.Text);
+            tempo = Double.Parse(txtTempo.Text);
+
+            resultado = capital * tempo / juros; 
+
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void btnResultado_MouseEnter(object sender, EventArgs e)
+        {
+            btnResultado.ForeColor = Color.FromArgb(230, 57, 70);
+            btnResultado.FlatAppearance.BorderColor = Color.FromArgb(230, 57, 70);
+        }
+
+        private void btnResultado_MouseLeave(object sender, EventArgs e)
+        {
+            btnResultado.ForeColor = Color.White;
+            btnResultado.FlatAppearance.BorderColor = Color.White;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPercent_MouseEnter(object sender, EventArgs e)
+        {
+            btnPercent.ForeColor = Color.FromArgb(230, 57, 70);
+            btnPercent.FlatAppearance.BorderColor = Color.FromArgb(230, 57, 70);
+        }
+
+        private void btnPercent_MouseLeave(object sender, EventArgs e)
+        {
+            btnPercent.ForeColor = Color.White;
+            btnPercent.FlatAppearance.BorderColor = Color.White;
+        }
+
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+            resultado = resultado / 100;
+            txtResultado.Text = resultado.ToString() + "%";
+        }
     }
 }
